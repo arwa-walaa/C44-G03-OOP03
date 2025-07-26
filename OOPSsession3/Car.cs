f24c41ee0d738a7f327b0206d6188ba8eb5bd9ae
arwa-walaa <84721941+arwa-walaa@users.noreply.github.com>
2025-07-26 14:54:30 +0300

    Class

---
+++ OOPSsession3/Car.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSsession3
+{
+     class Car
+    {
+        #region Attributes
+        //private int id;
+        //private string? model;
+        //private double speed;
+
+        #endregion
+
+
+        #region Properties
+        public int id { get; set; }
+        public string? model { get; set; }
+        public double speed { get; set; }
+        #endregion
+
+        #region Methods
+        public override string ToString()
+        {
+            return $"Car ID: {id}, Model: {model}, Speed: {speed} km/h";
+        }
+        #endregion
+        #region Constructors
+      
+        public Car(int id, string model, double speed)
+        {
+            this.id = id;
+            this.model = model;
+            this.speed = speed;
+            Console.WriteLine("1st CTOR");
+        }
+
+        public Car(int id, string model): this(id, model, 360)
+        {
+           Console.WriteLine("2nd CTOR");
+        }
+
+        public Car(int id) : this(id, "BMW", 360)
+        {
+            Console.WriteLine("3rd CTOR");
+        }
+        #endregion
+
+
+    }
+}
--- OOPSsession3/Program.cs
+++ OOPSsession3/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region User Defiended Data Type[Class]
+            //Car c1=new Car(1,"BMW",300);
+            //Console.WriteLine(c1);
+
+           //Car c2= new Car(2, "Audi");
+           // Console.WriteLine(c2);
+
+            Car c3 = new Car(3);
+            Console.WriteLine(c3);
+
+
+            #endregion
         }
     }
 }
